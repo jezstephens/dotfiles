ac1326af27524dec3c6399aef7cdd589fa0aa9a3
Jez Stephens <jezstephens@gmail.com>
2012-02-11 22:26:29 +0000

    Add xmonad and xmobar configs

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,63 @@
+import XMonad
+import XMonad.Actions.UpdatePointer
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
+import XMonad.Util.Run
+
+import qualified Data.Map as M
+
+
+myModMask = mod4Mask
+
+myWorkspaces = map show [1..8]
+
+myFocusedBorderColor = "#3399ff"
+
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+    -- default tiling algorithm partitions the screen into two panes
+    tiled   = Tall nmaster delta ratio
+    -- The default number of windows in the master pane
+    nmaster = 1
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
+    -- Percent of screen to increment by when resizing panes
+    delta   = 3/100
+
+
+myKeys (XConfig {modMask = modm}) = M.fromList $
+       [ ((modm .|. shiftMask, xK_i), spawn "chromium")
+       , ((modm, xK_i), spawn "uzbl-tabbed about:blank")
+       , ((modm, xK_u), spawn "gvim")
+       , ((modm .|. shiftMask, xK_u), spawn "emacs")
+       , ((modm .|. shiftMask, xK_z), spawn "slock") ]
+
+
+myPP bar = defaultPP 
+  { ppOutput = hPutStrLn bar
+  , ppTitle = xmobarColor "white" "" . shorten 110
+  , ppCurrent = xmobarColor "white" "black" . pad
+  , ppHidden = pad
+  , ppHiddenNoWindows = \w -> xmobarColor "#444" "" (" " ++ w ++ " ")
+  , ppSep = xmobarColor "#555" "" " / "
+  , ppWsSep = ""
+  , ppLayout = \x -> case x of
+    "Tall"        -> "T"
+    "Mirror Tall" -> "M"
+    "Full"        -> "F"
+    _             -> "?"
+  }
+
+main = do
+  xmobar <- spawnPipe "xmobar ~/.xmobarrc"
+  xmonad $ defaultConfig
+    { modMask            = myModMask
+    , keys               = \c -> myKeys c `M.union` keys defaultConfig c
+    -- , keys               = \c -> myKeys c `M.union` keys gnomeConfig c
+    , workspaces         = myWorkspaces
+    , layoutHook         = smartBorders (avoidStruts myLayout)
+    , focusedBorderColor = myFocusedBorderColor
+    , terminal           = "urxvtc"
+    , logHook            = dynamicLogWithPP (myPP xmobar) >> updatePointer (Relative 0.5 0.5)
+    }
