633dfb483590fd7e1b8456451236c27511ba5d65
Jez Stephens <jezstephens@gmail.com>
2013-05-12 13:53:07 +0100

    update zsh prompt

--- .zsh.d/theme.zsh
+++ .zsh.d/theme.zsh
@@ -1,6 +1,6 @@
-PROMPT='%{$fg[white]%}%~ ${$(git_prompt_info):-»}%{$reset_color%} '
+PROMPT='%{$fg[white]%}%~%{$reset_color%} ${$(git_prompt_info):-»}%{$reset_color%} '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[cyan]%}("
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[cyan]%}) %{$fg[red]%}»%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[cyan]%})"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[cyan]%}) %{$reset_color%}»"
