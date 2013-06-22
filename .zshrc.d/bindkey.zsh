7ab2e04763e5196f3c31ae490eaca2330fd525a5
Jez Stephens <jezstephens@gmail.com>
2013-06-22 16:47:22 +0100

    Zsh bindings

--- .zshrc.d/bindkey.zsh
+++ .zshrc.d/bindkey.zsh
@@ -1,1 +1,8 @@
-bindkey -M vicmd '^H' run-help
+bindkey -M vicmd '^h' run-help
+bindkey -M vicmd '^r' history-incremental-pattern-search-backward
+bindkey -M vicmd '^s' history-incremental-pattern-search-forward
+bindkey -M vicmd '?'  history-search-backward
+bindkey -M vicmd '/'  history-search-forward
+
+bindkey -M viins '^r' history-incremental-pattern-search-backward
+bindkey -M viins '^s' history-incremental-pattern-search-forward
