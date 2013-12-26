9acf71d556f2bc9ec88329405c61f9f0da24c98c
Jez Stephens <jezstephens@gmail.com>
2013-12-26 17:21:08 +0000

    upgrade oh-my-zsh

--- .zshrc.d/oh-my-zsh.zsh
+++ .zshrc.d/oh-my-zsh.zsh
@@ -1,4 +1,4 @@
 ZSH=$HOME/.oh-my-zsh
 DISABLE_CORRECTION="true"
-plugins=(vi-mode git taskwarrior vagrant ruby gem bundler rails3)
+plugins=(vi-mode git taskwarrior vagrant ruby gem bundler rails node npm)
 source $ZSH/oh-my-zsh.sh
