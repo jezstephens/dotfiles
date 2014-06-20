01a165b5cdf8c61ca0d72a6ea4606c3431a8b54b
Jez Stephens <jezstephens@gmail.com>
2014-06-20 21:59:09 +0100

    some aliases

--- .zshrc.d/alias.zsh
+++ .zshrc.d/alias.zsh
@@ -1,6 +1,9 @@
-alias vi='vim'
 alias psl='ps -ely'
 alias rm='rm -i'
+alias vi='vim'
+alias svi='EDITOR=vim sudoedit'
+alias r='ranger'
+alias p='htop'
 
 if [[ $TERM == rxvt* ]]; then
   alias ssh='TERM=xterm-256color ssh'
@@ -11,3 +14,6 @@ alias gs='git status -sb'
 alias gems='gem search --remote'
 
 alias base='cd ~/src/maj/meet-and-jam'
+
+alias wifi-menu='sudo wifi-menu'
+alias wifi='wifi-menu'
