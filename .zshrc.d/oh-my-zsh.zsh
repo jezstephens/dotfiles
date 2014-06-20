0ee18ee42812bd0d8f281374bc17657a8c57aed3
Jez Stephens <jezstephens@gmail.com>
2014-06-20 21:58:59 +0100

    remove some zsh configs

--- .zshrc.d/oh-my-zsh.zsh
+++
@@ -1,4 +0,0 @@
-ZSH=$HOME/.oh-my-zsh
-DISABLE_CORRECTION="true"
-plugins=(vi-mode git taskwarrior vagrant ruby gem bundler rails node npm)
-source $ZSH/oh-my-zsh.sh
--- .zshrc.d/rails.zsh
+++
@@ -1,1 +0,0 @@
-alias rails='script/rails'
