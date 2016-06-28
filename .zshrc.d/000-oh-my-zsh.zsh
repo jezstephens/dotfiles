e4ed6ef1f561010c5a4568757914f466f30bbe3d
Jez Stephens <jezstephens@gmail.com>
2016-06-28 11:14:14 +0100

    commit oh-my-zsh config

---
+++ .zshrc.d/000-oh-my-zsh.zsh
@@ -0,0 +1,4 @@
+ZSH=$HOME/.oh-my-zsh
+DISABLE_CORRECTION="true"
+plugins=(vi-mode common-aliases fasd gitfast git-flow taskwarrior vagrant ruby gem bundler rails node npm command-not-found)
+source $ZSH/oh-my-zsh.sh
